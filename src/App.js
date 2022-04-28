482bb1960bf29639fda460fb926f62b1f2ce3d46
Fredson <85347233+fredsons@users.noreply.github.com>
2022-04-28 03:52:15 -0300

    ajustes nas dependencias e estrutura

--- src/App.js
+++ src/App.js
@@ -1,10 +1,37 @@
 
+import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import Home from "./components/pages/Home";
+import Company from "./components/pages/Company";
+import Contact from "./components/pages/Contact";
+import  NewProject from "./components/pages/NewProject";
 function App() {
   return (
-    <div className="App">
-      <p>Costs</p>
-    </div>
-  );
+    <Router>
+      <div>
+        <Link to="/">Home</Link>
+        <Link to="/contact">Contact</Link>
+        <Link to="/company">Company</Link>
+        <Link to="/newproject">NewProject</Link>
+        
+      </div>
+      <Switch>
+        <Route exact path="/">
+          <Home/>
+        </Route>
+        <Route path="/contact">
+          <Contact/>
+        </Route>
+        <Route path="/company">
+          <Company/>
+        </Route>
+        <Route path="/newproject">
+          <NewProject/>
+        </Route>
+      </Switch>
+      <p>Footer</p>
+    </Router>
+  )
+    
 }
 
 export default App;
---
+++ src/components/pages/Home.js
@@ -0,0 +1,9 @@
+function Home() {
+  return(
+
+    <h1>Ola</h1>
+  ) 
+    
+  
+}
+export default Home
